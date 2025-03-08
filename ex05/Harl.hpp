c305612f9249d8ed43a591011647ea3bc7944973
Ilan Nowak--Glandier <inowak--@2C7.42angouleme.fr>
2025-03-08 19:27:44 +0100

    raccourci

---
+++ ex05/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inowak-- <inowak--@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/08 14:49:56 by inowak--          #+#    #+#             */
+/*   Updated: 2025/03/08 18:37:26 by inowak--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl{
+  
+	public :
+
+	private :
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+
+};
+
+
+#endif
